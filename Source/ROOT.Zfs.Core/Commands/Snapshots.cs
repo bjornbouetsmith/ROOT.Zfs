4ff58f621878dec747ecb1dc65075105ff3cfb4c
bbs <bjornsmith@gmail.com>
2020-10-21 20:06:48 +0200

    add packaging information - expand on Snapshot

---
+++ Source/ROOT.Zfs.Core/Commands/Snapshots.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using ROOT.Shared.Utils.OS;
+using ROOT.Zfs.Core.Info;
+
+namespace ROOT.Zfs.Core.Commands
+{
+    public static class SnapshotParser
+    {
+        public static IEnumerable<Snapshot> Parse(string snapshotResponse)
+        {
+            foreach(var line in snapshotResponse.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries))
+            {
+                yield return Snapshot.FromString(line);
+            }
+        }
+    }
+
+    public class Snapshots
+    {
+        public IEnumerable<Snapshot> LoadSnapshots(string dataset)
+        {
+            ProcessCall pc = new ProcessCall("/sbin/zfs", $"list -H -t snapshot -p -o creation,name,used -d 1 -r {dataset}");
+            var response = pc.LoadResponse();
+            if (response.Success)
+            {
+                Console.WriteLine($"Command: {pc.FullCommandLine} success");
+                return SnapshotParser.Parse(response.StdOut);
+            }
+
+            throw response.ToException();
+        }
+
+        private static readonly Regex NameAllow = new Regex("[0-9]|[a-z]|[A-Z]|_|-");
+
+        public void DestroySnapshot(string dataset, string snapName)
+        {
+            if (NameAllow.Matches(snapName).Count != snapName.Length)
+            {
+                throw new ArgumentException($"{snapName} is not a valid snapshot name - valid characters are [0-9]|[a-z]|[A-Z]|_|-", nameof(snapName));
+            }
+
+            ProcessCall pc = new ProcessCall("/sbin/zfs", $"destroy {dataset}@{snapName}");
+            var response = pc.LoadResponse();
+            if (response.Success)
+            {
+                Console.WriteLine($"Command: {pc.FullCommandLine} success");
+            }
+            else
+            {
+                throw response.ToException();
+            }
+
+        }
+
+        public void CreateSnapshot(string dataset)
+        {
+            CreateSnapshot(dataset, DateTime.UtcNow.ToString("yyyyMMddhhmmss"));
+        }
+
+        public void CreateSnapshot(string dataset, string snapName)
+        {
+            if (NameAllow.Matches(snapName).Count != snapName.Length)
+            {
+                throw new ArgumentException($"{snapName} is not a valid snapshot name - valid characters are [0-9]|[a-z]|[A-Z]|_|-", nameof(snapName));
+            }
+
+            ProcessCall pc = new ProcessCall("/sbin/zfs", $"snap {dataset}@{snapName}");
+            var response = pc.LoadResponse();
+            if (response.Success)
+            {
+                Console.WriteLine($"Command: {pc.FullCommandLine} success");
+            }
+            else
+            {
+                throw response.ToException();
+            }
+        }
+    }
+}
---
+++ Source/ROOT.Zfs.Tests/SnapshotTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ROOT.Shared.Utils.Serialization;
+using ROOT.Zfs.Core.Commands;
+
+namespace ROOT.Zfs.Tests
+{
+    [TestClass]
+    public class SnapshotTest
+    {
+        private const string SnapshotList = @"1590930547      nvme/nfs@auto-20200531.1509-2w  5165371392
+1591016947      nvme/nfs@auto-20200601.1509-2w  2669617152
+1591103347      nvme/nfs@auto-20200602.1509-2w  2752487424
+
+1591189747      nvme/nfs@auto-20200603.1509-2w  2914050048
+1591276147      nvme/nfs@auto-20200604.1509-2w  3080941568
+";
+
+
+        [TestMethod]
+        public void SnapshotParserTest()
+        {
+            var list = SnapshotParser.Parse(SnapshotList).ToList();
+
+            Assert.AreEqual(5, list.Count);
+
+            foreach (var snap in list)
+            {
+                Console.WriteLine(snap.CreationDate.AsString());
+                Console.WriteLine(snap.Name);
+                Console.WriteLine(snap.Size.AsString());
+            }
+        }
+    }
+}
