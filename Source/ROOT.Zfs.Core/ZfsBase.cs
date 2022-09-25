4c0209ebc26825a7356f3449f6f24ad06614ff68
Bjørn Smith <bjornsmith@gmail.com>
2022-09-25 17:57:32 +0200

    Better utils where you can pipe null ProcessCalls

--- Source/ROOT.Zfs.Core/ZfsBase.cs
+++ Source/ROOT.Zfs.Core/ZfsBase.cs
@@ -17,19 +17,11 @@ namespace ROOT.Zfs.Core
 
         protected ProcessCall BuildCommand(ProcessCall current, ProcessCall previousCall = null)
         {
-            ProcessCall command = null;
-
             // First use any remote connection
-            if (_remoteConnection != null)
-            {
-                command = _remoteConnection;
-            }
-
+            ProcessCall command = _remoteConnection;
+            
             // pipe into previous call if any
-            if (previousCall != null)
-            {
-                command |= previousCall;
-            }
+            command |= previousCall;
 
             // finally make the command complete, by piping into the current command
             command |= current;
