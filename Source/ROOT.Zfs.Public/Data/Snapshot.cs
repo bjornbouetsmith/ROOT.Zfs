84181d131b5b7964311941bd4ae0998a03f8e307
Bjørn Smith <bjornsmith@gmail.com>
2022-10-04 20:22:16 +0200

    Remove ctor never used

--- Source/ROOT.Zfs.Public/Data/Snapshot.cs
+++ Source/ROOT.Zfs.Public/Data/Snapshot.cs
@@ -7,16 +7,5 @@ namespace ROOT.Zfs.Public.Data
         public string Name { get; set; }
         public DateTime CreationDate { get; set; }
         public long Size { get; set; }
-
-        public Snapshot()
-        {
-        }
-
-        public Snapshot(string name, long size, DateTime creationDate)
-        {
-            Name = name;
-            Size = size;
-            CreationDate = creationDate;
-        }
     }
 }
