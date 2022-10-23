ed1b23ca6ea540ad121af1bce068d219946b8f8c
Bjørn Smith <bjornsmith@gmail.com>
2022-10-23 09:41:50 +0200

    Document vdev/draid arguments sparingly #4

--- Source/ROOT.Zfs.Public/Data/Pools/DraigVDevCreationArgs.cs
+++ Source/ROOT.Zfs.Public/Data/Pools/DraigVDevCreationArgs.cs
@@ -1,9 +1,19 @@
 ﻿namespace ROOT.Zfs.Public.Data.Pools
 {
+    /// <summary>
+    /// Specialized version of <see cref="VDevCreationArgs" /> for creating draid
+    /// </summary>
     public class DraigVDevCreationArgs : VDevCreationArgs
     {
+        /// <summary>
+        /// Arguments for the draid
+        /// </summary>
         public DraidArgs DraidArgs { get; set; }
 
+        /// <summary>
+        /// Returns the draid specialized string version of this instance
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return $"{Type.AsString()}:{DraidArgs.DataDevices}d:{DraidArgs.Children}c:{DraidArgs.Spares}s {string.Join(' ', Devices)}";
--- Source/ROOT.Zfs.Public/Data/Pools/VDevCreationArgs.cs
+++ Source/ROOT.Zfs.Public/Data/Pools/VDevCreationArgs.cs
@@ -2,16 +2,34 @@
 
 namespace ROOT.Zfs.Public.Data.Pools
 {
+    /// <summary>
+    /// Arguments for specifying a vdev when creating a pool
+    /// </summary>
     public class VDevCreationArgs
     {
+        /// <summary>
+        /// The type of vdev to create
+        /// </summary>
         public VDevCreationType Type { get; set; }
+        
+        /// <summary>
+        /// The list of devices that should be part of this vdev
+        /// </summary>
         public IList<string> Devices { get; set; }
 
+        /// <summary>
+        /// Converts this instance into what is required for zpool create
+        /// </summary>
         public override string ToString()
         {
             return $"{Type.AsString()} {string.Join(' ', Devices)}";
         }
 
+        /// <summary>
+        /// Validates that the vdev arguments are correct and will not cause runtime errors when passed onto to zpool
+        /// </summary>
+        /// <param name="errorMessage">The error message if any</param>
+        /// <returns>true if valid;false otherwise</returns>
         public virtual bool Validate(out string errorMessage)
         {
             //TODO: Write validation logic, i.e. mirror needs at least two devices etc.
