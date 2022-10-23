bfea90b8fea985ef3ca6b58f761977842a3623ed
Bjørn Smith <bjornsmith@gmail.com>
2022-10-23 10:00:01 +0200

    Make VDevCreationType to string easier to maintain, and write tests #5

--- Source/ROOT.Zfs.Public/Data/EnumExt.cs
+++ Source/ROOT.Zfs.Public/Data/EnumExt.cs
@@ -1,45 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 using ROOT.Zfs.Public.Data.Pools;
 
 namespace ROOT.Zfs.Public.Data
 {
+    /// <summary>
+    /// Enum extension methods
+    /// </summary>
     public static class EnumExt
     {
+        private static readonly Dictionary<VDevCreationType, string> _VdevMappings = new Dictionary<VDevCreationType, string> 
+            {
+            {VDevCreationType.Cache,"cache"},
+            {VDevCreationType.Dedup,"dedup"},
+            {VDevCreationType.DRaid1,"draid1"},
+            {VDevCreationType.DRaid2,"draid2"},
+            {VDevCreationType.DRaid3,"draid3"},
+            {VDevCreationType.Log,"log"},
+            {VDevCreationType.Mirror,"mirror"},
+            {VDevCreationType.Raidz1,"raidz1"},
+            {VDevCreationType.Raidz2,"raidz2"},
+            {VDevCreationType.Raidz3,"raidz3"},
+            {VDevCreationType.Spare,"spare"},
+            {VDevCreationType.Special,"special"},
+            };
+
         /// <summary>
         /// Returns string representation of VDevCreationType as required by zpool create
         /// </summary>
-        /// <exception cref="NotImplementedException">If you pass some value not yet supported</exception>
+        /// <exception cref="NotSupportedException">If you pass some value not yet supported</exception>
         public static string AsString(this VDevCreationType type)
         {
-            switch (type)
+            if (!_VdevMappings.TryGetValue(type, out var stringVer))
             {
-                case VDevCreationType.Cache:
-                    return "cache";
-                case VDevCreationType.Dedup:
-                    return "dedup";
-                case VDevCreationType.DRaid1:
-                    return "draid1";
-                case VDevCreationType.DRaid2:
-                    return "draid2";
-                case VDevCreationType.DRaid3:
-                    return "draid3";
-                case VDevCreationType.Log:
-                    return "log";
-                case VDevCreationType.Mirror:
-                    return "mirror";
-                case VDevCreationType.Raidz1:
-                    return "raidz1";
-                case VDevCreationType.Raidz2:
-                    return "raidz2";
-                case VDevCreationType.Raidz3:
-                    return "raidz3";
-                case VDevCreationType.Spare:
-                    return "spare";
-                case VDevCreationType.Special:
-                    return "special";
-                default:
-                    throw new NotImplementedException($"Please implement AsString for {type} and ensure nothing is broken");
+                throw new NotSupportedException($"Please implement AsString for {type} and ensure nothing is broken");
             }
+
+            return stringVer;
         }
     }
 }
---
+++ Source/ROOT.Zfs.Tests/Public/Data/EnumExt.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ROOT.Zfs.Public.Data;
+using ROOT.Zfs.Public.Data.Pools;
+
+namespace ROOT.Zfs.Tests.Public.Data
+{
+    [TestClass]
+    public class EnumExt
+    {
+
+        /// <summary>
+        /// Test to ensure that all all <see cref="VDevCreationType"/> are handled properly
+        /// </summary>
+        [TestMethod]
+        public void ReflectionBasedTest()
+        {
+            foreach (var type in Enum.GetValues(typeof(VDevCreationType)).OfType<VDevCreationType>())
+            {
+                var s = type.AsString();
+                Console.WriteLine("{0}={1}", type, s);
+                Assert.IsNotNull(s);
+            }
+        }
+        /// <summary>
+        /// Known <see cref="VDevCreationType"/> to string mappings
+        /// </summary>
+        [DataRow(VDevCreationType.Special,"special")]
+        [DataRow(VDevCreationType.Cache, "cache")]
+        [DataRow(VDevCreationType.Dedup, "dedup")]
+        [DataRow(VDevCreationType.DRaid1, "draid1")]
+        [DataRow(VDevCreationType.DRaid2, "draid2")]
+        [DataRow(VDevCreationType.DRaid3, "draid3")]
+        [DataRow(VDevCreationType.Log, "log")]
+        [DataRow(VDevCreationType.Mirror, "mirror")]
+        [DataRow(VDevCreationType.Raidz1, "raidz1")]
+        [DataRow(VDevCreationType.Raidz2, "raidz2")]
+        [DataRow(VDevCreationType.Raidz3, "raidz3")]
+        [DataRow(VDevCreationType.Spare, "spare")]
+        [TestMethod]
+        public void VDevCreationTypeAsStringTest(VDevCreationType type, string expected)
+        {
+            var s = type.AsString();
+            Assert.AreEqual(expected, s);
+        }
+
+        [TestMethod]
+        public void UnsupportedShouldThrow()
+        {
+            VDevCreationType type = (VDevCreationType) (int)-999;
+
+            var ex = Assert.ThrowsException<NotSupportedException>(() => type.AsString());
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
