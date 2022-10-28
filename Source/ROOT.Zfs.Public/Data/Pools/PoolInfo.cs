d52838155cae971fdbeadb4e4b239aac4839c871
Bjørn Smith <bjornsmith@gmail.com>
2022-10-28 23:16:05 +0200

    Document pool info and add version

--- Source/ROOT.Zfs.Public/Data/Pools/PoolInfo.cs
+++ Source/ROOT.Zfs.Public/Data/Pools/PoolInfo.cs
@@ -1,19 +1,60 @@
-﻿using System.Collections.Generic;
-using ROOT.Zfs.Public.Data.Statistics;
+﻿using ROOT.Zfs.Public.Data.Statistics;
 
 namespace ROOT.Zfs.Public.Data.Pools
 {
+    /// <summary>
+    /// Contains information about a pool, output from zpool list
+    /// </summary>
     public class PoolInfo
     {
+        /// <summary>
+        /// The underlying version of the pool
+        /// </summary>
+        public int Version { get; set; }
         
+        /// <summary>
+        /// The name of the pool
+        /// </summary>
         public string Name { get; set; }
+        
+        /// <summary>
+        /// Total size of the pool
+        /// </summary>
         public Size Size { get; set; }
+
+        /// <summary>
+        /// Allocated bytes in the pool
+        /// </summary>
         public Size Allocated { get; set; }
+
+        /// <summary>
+        /// Free bytes in the pool
+        /// </summary>
         public Size Free { get; set; }
+
+        /// <summary>
+        /// Fragmentation percentage
+        /// </summary>
         public Part Fragmentation { get; set; }
+
+        /// <summary>
+        /// Used space in the pool
+        /// </summary>
         public Part CapacityUsed { get; set; }
+
+        /// <summary>
+        /// Dedup ratio, if deduplication is enabled
+        /// </summary>
         public Ratio DedupRatio { get; set; }
+
+        /// <summary>
+        /// State of the pool
+        /// </summary>
         public State State { get; set; }
+
+        /// <summary>
+        /// Alternative root for the pool
+        /// </summary>
         public string AltRoot { get; set; }
     }
 }
