088e8a8a9f67407558cf6757049267696cc3ab1f
Bjørn Smith <bjornsmith@gmail.com>
2022-11-02 17:52:08 +0100

    Get rid of property sources as a static class #3

--- Source/ROOT.Zfs.Core/Helpers/PropertyValueHelper.cs
+++ Source/ROOT.Zfs.Core/Helpers/PropertyValueHelper.cs
@@ -13,9 +13,9 @@ namespace ROOT.Zfs.Core.Helpers
                 throw new FormatException($"{line} could not be parsed, expected 4 parts, got: {parts.Length} - requires an output of NAME\\tPROPERTY\\tVALUE\\tSOURCE to be used for property list i.e. zfs get all -H");
             }
 
-            var property = parts[1];
-            var value = parts[2];
-            var source = PropertySources.Lookup(parts[3]);
+            var property = parts[1].Trim();
+            var value = parts[2].Trim();
+            var source = parts[3].Trim();
             return new PropertyValue { Property = property, Source = source, Value = value };
         }
     }
