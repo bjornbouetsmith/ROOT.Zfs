6fd8bb8e6b85f79151c7a597852de6317dc73a41
Bjørn Bouet Smith <bjornsmith@gmail.com>
2022-12-02 12:37:07 +0100

    Simplify code

--- Source/ROOT.Zfs.Core/Helpers/CommandHistoryHelper.cs
+++ Source/ROOT.Zfs.Core/Helpers/CommandHistoryHelper.cs
@@ -13,31 +13,28 @@ namespace ROOT.Zfs.Core.Helpers
             foreach (var line in stdOut.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries).Skip(1 + skipLines))
             {
                 var dateEnd = line.IndexOf(' ');
-                if (dateEnd > 0)
+
+                var command = string.Empty;
+                var caller = string.Empty;
+                var datePart = line.Substring(0, dateEnd);
+                var date = DateTime.ParseExact(datePart, "yyyy-MM-dd.HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal);
+                if (date <= afterDate)
                 {
-                    var command = string.Empty;
-                    var caller = string.Empty;
-                    var datePart = line.Substring(0, dateEnd);
-                    var date = DateTime.ParseExact(datePart, "yyyy-MM-dd.HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.AssumeLocal);
-                    if (date <= afterDate)
-                    {
-                        continue;
-                    }
-
-                    var indexOfBracket = line.IndexOf('[', dateEnd);
-                    if (indexOfBracket > 0)
-                    {
-                        var commandStart = dateEnd + 1;
-                        var commandEnd = indexOfBracket - 1; // Remove whitespace
-                        command = line[commandStart..commandEnd];
-                        var indexOfEndBracket = line.IndexOf(']', dateEnd);
-                        var callerStart = indexOfBracket + 1;
-                        caller = line[callerStart..indexOfEndBracket];
-                    }
-
-                    yield return new CommandHistory { Time = date, Command = command, Caller = caller };
+                    continue;
                 }
 
+                var indexOfBracket = line.IndexOf('[', dateEnd);
+                if (indexOfBracket > 0)
+                {
+                    var commandStart = dateEnd + 1;
+                    var commandEnd = indexOfBracket - 1; // Remove whitespace
+                    command = line[commandStart..commandEnd];
+                    var indexOfEndBracket = line.IndexOf(']', dateEnd);
+                    var callerStart = indexOfBracket + 1;
+                    caller = line[callerStart..indexOfEndBracket];
+                }
+
+                yield return new CommandHistory { Time = date, Command = command, Caller = caller };
             }
         }
     }
